319e614d16dd4eb5ddf25488b05e258e4dac9046
Muhammad Faisal <muhammadfaisalcs@gmail.com>
2019-09-29 19:22:17 +0500

    Create download_models.sh

---
+++ models/download_models.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download the EpO (motion only network)
+wget https://drive.google.com/file/d/1LxIyiHPoR5gIjs4bsZMtktsfPqJ1CZ8B/view?usp=sharing
+
+# Download the EpO+ (our Fusion Network)
+wget https://drive.google.com/file/d/18u8lIiO4i1QD65XNvZI-mxjUrrRbwPrs/view?usp=sharing
+
+# Download the EpO+ (our Fusion Network)
+wget https://drive.google.com/file/d/1tBfS5JTrx5bqaQaF5kwxhTg1Zc1_E2iR/view?usp=sharing
