5d4904b8cd6733ab9f88cebbdacd3c8cf2bdce8a
Muhammad Faisal <muhammadfaisalcs@gmail.com>
2019-09-29 19:22:11 +0500

    Create download_data.sh

---
+++ DAVIS_Dataset/download_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download JPEGImages (two Sequences - Camel & Car-Roundabout)
+wget https://drive.google.com/drive/folders/1tYtkCbhYNZk0O1TDayFBYOSyMviwfcoO?usp=sharing
+
+# Download Epipolar Score 
+wget https://drive.google.com/drive/folders/1gvMmAarNLfru7IVYkzfXuekhCMjcjYnO?usp=sharing
+
+# Download Optical Flow
+wget https://drive.google.com/drive/folders/1gvMmAarNLfru7IVYkzfXuekhCMjcjYnO?usp=sharing
+
+# Download Motion Images
+wget https://drive.google.com/drive/folders/1gvMmAarNLfru7IVYkzfXuekhCMjcjYnO?usp=sharing
