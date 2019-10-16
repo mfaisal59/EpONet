5e4290a6a2b410fe543dbf225dc4b9635c8570c0
Muhammad Faisal <muhammadfaisalcs@gmail.com>
2019-10-16 13:30:06 +0500

    update links

--- models/download_models.sh
+++ models/download_models.sh
@@ -3,7 +3,7 @@
 # Download the EpO (motion only network)
 wget https://drive.google.com/file/d/1LxIyiHPoR5gIjs4bsZMtktsfPqJ1CZ8B/view?usp=sharing
 
-# Download the EpO+ (our Fusion Network)
+# Download the DeepLab (AppeaNetwork Network)
 wget https://drive.google.com/file/d/18u8lIiO4i1QD65XNvZI-mxjUrrRbwPrs/view?usp=sharing
 
 # Download the EpO+ (our Fusion Network)
