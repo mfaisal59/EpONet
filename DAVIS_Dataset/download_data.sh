7e842452004bac6b70d7ba81c86f7db67323a5dd
Muhammad Faisal <muhammadfaisalcs@gmail.com>
2019-11-22 16:30:14 +0500

    Update Download Links

--- DAVIS_Dataset/download_data.sh
+++ DAVIS_Dataset/download_data.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+# function copied from https://gist.github.com/iamtekeste/3cdfd0366ebfd2c0d805
+gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
+    "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+
 # Download JPEGImages (two Sequences - Camel & Car-Roundabout)
-wget https://drive.google.com/drive/folders/1tYtkCbhYNZk0O1TDayFBYOSyMviwfcoO?usp=sharing
+fileid=1tYtkCbhYNZk0O1TDayFBYOSyMviwfcoO
+filename=JPEGImages.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
 
 # Download Epipolar Score 
-wget https://drive.google.com/drive/folders/1gvMmAarNLfru7IVYkzfXuekhCMjcjYnO?usp=sharing
+fileid=1KcxHUk1TCPCnWavXfId7lQPJXpO_dFd2
+filename=Epipolar.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
 
 # Download Optical Flow
-wget https://drive.google.com/drive/folders/1gvMmAarNLfru7IVYkzfXuekhCMjcjYnO?usp=sharing
+fileid=15yU5qsyuyq691MFZ8OE38IMunrwitK8S
+filename=OpticalFlow.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
 
 # Download Motion Images
-wget https://drive.google.com/drive/folders/1gvMmAarNLfru7IVYkzfXuekhCMjcjYnO?usp=sharing
+fileid=1LoTiOcsLsTqeNVr4rLWRAG1ng2p-Lh1e
+filename=motionImages.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
--- models/download_models.sh
+++ models/download_models.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-# Download the EpO (motion only network)
-wget https://drive.google.com/file/d/1LxIyiHPoR5gIjs4bsZMtktsfPqJ1CZ8B/view?usp=sharing
+#Download the pre-trained models
+
+# function copied from https://gist.github.com/iamtekeste/3cdfd0366ebfd2c0d805
+gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
+    "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+
+# Download EpO (motion only network)
+fileid=1LxIyiHPoR5gIjs4bsZMtktsfPqJ1CZ8B
+filename=EpO.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
 
 # Download the DeepLab (Appearance Network)
-wget https://drive.google.com/file/d/18u8lIiO4i1QD65XNvZI-mxjUrrRbwPrs/view?usp=sharing
+fileid=18u8lIiO4i1QD65XNvZI-mxjUrrRbwPrs
+filename=DeepLab.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
 
 # Download the EpO+ (our Fusion Network)
-wget https://drive.google.com/file/d/1tBfS5JTrx5bqaQaF5kwxhTg1Zc1_E2iR/view?usp=sharing
+fileid=1tBfS5JTrx5bqaQaF5kwxhTg1Zc1_E2iR
+filename=EpOPlus.tar.gz
+gdrive_download "$fileid" "$filename"
+tar -xvzf "$filename"
